2535435822e2e8f9ca237004244e8586deb095a2
Sean Twiehaus <sean.twie03@gmail.com>
2019-04-07 17:27:46 -0500

    InitialCommit

---
+++ 2019-04-07-Interface_Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2019_04_07_Interface_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
